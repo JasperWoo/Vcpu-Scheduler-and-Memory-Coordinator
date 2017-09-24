f8f9bb40f8acaeaee158c02c6aaca8eb58c0f69d
jasperwoo <jasperwoo516@gmail.com>
2017-09-24 10:14:59 -0400

    Project delivered

---
+++ CPU/vcpu_scheduler.c
@@ -0,0 +1,211 @@
+//
+// Created by pwu on 9/14/17.
+//
+#include <stdio.h>
+#include <libvirt/libvirt.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <float.h>
+
+static const char DBG_PWU = 1;
+static const long NANOSECOND = 1000000000;
+static const double DIFF_THRESHOLD = 20.0;
+
+struct DomList {
+    virDomainPtr *doms;
+    int n_doms;
+};
+
+struct DomStats {
+    virDomainPtr dom;
+    int n_vcpus;
+    unsigned long long *vcpus_time;
+    int *pcpus;
+};
+
+struct Vcpu {
+    virDomainPtr dom;
+    int vcpu_num;
+};
+
+
+int activeDoms(virConnectPtr conn, struct DomList *list) {
+    unsigned int flags = VIR_CONNECT_LIST_DOMAINS_ACTIVE |
+                         VIR_CONNECT_LIST_DOMAINS_RUNNING;
+    virDomainPtr *doms;
+    int n_doms = virConnectListAllDomains(conn, &doms, flags);
+    if (n_doms == 0) {
+        printf("[ERROR] No active domains!\n");
+        exit(EXIT_FAILURE);
+    }
+    list->doms = doms;
+    list->n_doms = n_doms;
+    if (DBG_PWU) {
+        printf("we have %d active domains\n", n_doms);
+    }
+    return n_doms;
+}
+
+unsigned int getNrPcpus(virConnectPtr conn) {
+    virNodeInfo info;
+    virNodeGetInfo(conn, &info);
+    return info.cpus;
+}
+
+void setDomStats(int n_pcpus, struct DomList *dom_list, struct DomStats *dom_stats) {
+    virVcpuInfoPtr info_vcpu;
+    unsigned char *cpumaps;
+    size_t len_cpumap;
+    for (int i = 0; i < dom_list->n_doms; i++) {
+        int n_vcpus = 0;
+        if ((n_vcpus = virDomainGetVcpusFlags(dom_list->doms[i], VIR_DOMAIN_AFFECT_LIVE)) == -1) {
+            printf("[ERROR] Domain status changed during calculation.\n");
+        }
+        dom_stats[i].vcpus_time = (unsigned long long *)calloc(n_vcpus, sizeof(unsigned long long));
+        dom_stats[i].pcpus = (int *)calloc(n_vcpus, sizeof(int));
+        info_vcpu = (virVcpuInfoPtr)calloc(n_vcpus, sizeof(virVcpuInfo));
+        len_cpumap = VIR_CPU_MAPLEN(n_pcpus);
+        cpumaps = (unsigned char *)calloc(n_vcpus, len_cpumap);
+        if (virDomainGetVcpus(dom_list->doms[i], info_vcpu, n_vcpus, cpumaps, len_cpumap) == -1) {
+            printf("[ERROR] Could not retrieve vCpus affinity info\n");
+        }
+        for (int j = 0; j < n_vcpus; j++) {
+            dom_stats[i].vcpus_time[j] = info_vcpu[j].cpuTime; //TODO: Consider stores vcpu time and cpu to the slot indexed by "number" field.
+            dom_stats[i].pcpus[j] = info_vcpu[j].cpu;
+        }
+        dom_stats[i].dom = dom_list->doms[i];
+        dom_stats[i].n_vcpus = n_vcpus;
+    }
+
+    return;
+}
+
+double usage(unsigned long long int diff, double period) {
+    return diff / period * 100;
+}
+
+void clearPcpuUsage(double *pcpu_usage, int n_pcpus) {
+    for (int i = 0; i < n_pcpus; i++) {
+        pcpu_usage[i] = 0.0;
+    }
+    return;
+}
+
+void vcpuPin(double *pcpu_usage, int n_pcpus, struct DomStats *cur_dom_stats,
+             struct DomStats *prev_dom_stats, int n_doms, double period) {
+    int busiest_cpu;
+    int freest_cpu;
+    double busiest_usage = 0.0;
+    double freest_usage = DBL_MAX;
+    for (int i = 0; i < n_pcpus; i++) {
+        if (pcpu_usage[i] > busiest_usage) {
+            busiest_usage = pcpu_usage[i];
+            busiest_cpu = i;
+        }
+        if (pcpu_usage[i] < freest_usage) {
+            freest_usage = pcpu_usage[i];
+            freest_cpu = i;
+        }
+    }
+    size_t len_cpumap = VIR_CPU_MAPLEN(n_pcpus);
+    unsigned char determ_cpumap = 0x1;
+    if ((busiest_usage - freest_usage) < DIFF_THRESHOLD) {
+        printf("Balanced enough for diff threshold = %f.No need to change pinning, making deterministic pinning...\n", DIFF_THRESHOLD);
+        for (int i = 0; i < n_doms; i++) {
+            for (int j = 0; j < cur_dom_stats[i].n_vcpus; j++) {
+                determ_cpumap = 0x1 << cur_dom_stats[i].pcpus[j];
+                virDomainPinVcpu(cur_dom_stats[i].dom, j, &determ_cpumap, len_cpumap);
+            }
+        }
+        return;
+    }
+    printf("Freest cpu %d has usage %f,\n Busiest cpu %d has usage %f.\n", freest_cpu, freest_usage,
+        busiest_cpu, busiest_usage);
+    unsigned char freest_cpumap = 0x1 << freest_cpu;
+    struct Vcpu lightest_vcpu = {0};  //TODO: check if this initializes..
+    double lightest_usage = DBL_MAX;
+    double use = 0.0;
+    for (int i = 0; i < n_doms; i++) {
+        for (int j = 0; j < cur_dom_stats[i].n_vcpus; j++) {
+            if (cur_dom_stats[i].pcpus[j] == busiest_cpu) {
+                use =  usage(cur_dom_stats[i].vcpus_time[j] -
+                             prev_dom_stats[i].vcpus_time[j], period);
+                if (use < lightest_usage) {
+                    lightest_usage = use;
+                    lightest_vcpu.dom = cur_dom_stats[i].dom;
+                    lightest_vcpu.vcpu_num = j;
+                }
+            }
+        }
+    }
+    printf("Pinning vcpu %d in domain %s to cpu %d ....\n", lightest_vcpu.vcpu_num,
+           virDomainGetName(lightest_vcpu.dom), freest_cpu);
+    if (virDomainPinVcpu(lightest_vcpu.dom, lightest_vcpu.vcpu_num, &freest_cpumap, len_cpumap) == 0) {
+        printf("Pinning finished!\n");
+    } else {
+        printf("Pinning failed!\n");
+    };
+
+    return;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("[ERROR] Usage: vcpu_scheduler <time interval(s)>.\n");
+        return 1;
+    }
+    virConnectPtr conn = virConnectOpen("qemu:///system");
+    int prev_n_doms = 0;
+    int n_pcpus = getNrPcpus(conn);
+    struct DomList* dom_list = (struct DomList *)calloc(1, sizeof(struct DomList)); //TODO: free
+    double *pcpu_usage = (double *)calloc(n_pcpus, sizeof(double)); //TODO:free
+    if (!dom_list || !pcpu_usage) {
+        printf("[ERROR] Cannot allocate memory.\n");
+        return(1);
+    }
+
+    struct DomStats *cur_dom_stats, *prev_dom_stats; //TODO: free inside and outside.
+    double period = atof(argv[1]) * NANOSECOND;
+    while (activeDoms(conn, dom_list) > 0) {
+        // Set up current round data.
+        cur_dom_stats = (struct DomStats *)calloc(dom_list->n_doms, sizeof(struct DomStats)); //TODO: free
+        setDomStats(n_pcpus, dom_list, cur_dom_stats);
+
+        // Check if # of active domain changes. When unchanged, calculate the usage and pin accordingly.
+        if (dom_list->n_doms != prev_n_doms) {
+            printf("Number of DOMs changed...\n");
+            prev_dom_stats = (struct DomStats *)calloc(dom_list->n_doms, sizeof(struct DomStats));
+        } else {
+            printf("Number of DOMs stays the same...Calculating...\n");
+            double use = 0.0;
+            for (int i = 0; i < dom_list->n_doms; i++) {
+                for (int j = 0; j < cur_dom_stats[i].n_vcpus; j++) {
+                    use =  usage(cur_dom_stats[i].vcpus_time[j] -
+                                                prev_dom_stats[i].vcpus_time[j], period);
+                    printf("Domain %s 's %d vcpu has usage %f during this period, \n",
+                           virDomainGetName(dom_list->doms[i]), j, use);
+                    pcpu_usage[cur_dom_stats[i].pcpus[j]] += use;
+                }
+            }
+            vcpuPin(pcpu_usage, n_pcpus, cur_dom_stats, prev_dom_stats, dom_list->n_doms, period);
+        } // finish checking dom number change.
+        printf("Finished scheduling!\n\n\n\n");
+        // Update prev value to cur.
+        memcpy(prev_dom_stats, cur_dom_stats, dom_list->n_doms * sizeof(struct DomStats));
+        prev_n_doms = dom_list->n_doms;
+        clearPcpuUsage(pcpu_usage, n_pcpus);
+        sleep(atoi(argv[1]));
+    } // finish while loop
+
+    virConnectClose(conn);
+
+    free(dom_list->doms);
+    free(dom_list);
+    printf("No active doms. Program exiting..\n");
+    return 0;
+}
+
+
+
+
